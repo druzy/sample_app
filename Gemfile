5e6580cbdb61aacc27921ae45a7047d055529bc0
Jonathan Le Greneur <jonathan.legreneur@free.fr>
2017-07-20 17:00:23 +0200

    Fini avec les pages statiques

--- Gemfile
+++ Gemfile
@@ -52,4 +52,5 @@ end
 group :test do
 	gem 'rspec'
 	gem 'webrat'
+	gem 'capybara'
 end
--- spec/controllers/pages_controller_spec.rb
+++ spec/controllers/pages_controller_spec.rb
@@ -1,11 +1,17 @@
 require 'rails_helper'
 
 RSpec.describe PagesController, type: :controller do
+  render_views
 
   describe "GET #home" do
     it "returns http success" do
       get :home
       expect(response).to have_http_status(:success)
+    end
+    
+    it "is the good title" do
+    	get :home
+    	expect(response.body).to have_title('Simple App du Tutoriel Ruby on Rails | Accueil')
     end
   end
 
@@ -13,6 +19,23 @@ RSpec.describe PagesController, type: :controller do
     it "returns http success" do
       get :contact
       expect(response).to have_http_status(:success)
+    end
+    
+    it "is the good title" do
+    	get :contact
+    	expect(response.body).to have_title('Simple App du Tutoriel Ruby on Rails | Contact')
+    end
+  end
+  
+  describe "GET #about" do
+    it "returns http success" do
+      get :about
+      expect(response).to have_http_status(:success)
+    end
+    
+    it "is the good title" do
+    	get :about
+    	expect(response.body).to have_title('Simple App du Tutoriel Ruby on Rails | À propos')
     end
   end
 
