6f72f9b3f61798df4336e1058b4e826721cd2486
Jonathan Le Greneur <jonathan.legreneur@free.fr>
2017-07-23 16:25:01 +0200

    before css

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,13 @@
 module ApplicationHelper
+
+  # Retourner un titre basé sur la page.
+  def title
+    base_title = "Simple App du Tutoriel Ruby on Rails"
+    if @title.nil?
+      return base_title
+    else
+      return "#{base_title} | #{@title}"
+    end
+  end
+  
 end
